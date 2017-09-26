7d546091f7844fab46a2ee66e5693915be500d03
matthewpdias <matthewpdias@gmail.com>
2017-09-26 12:58:39 -0700

    finished MWEB-1 multiple logins cause service1 to crash

--- make_commits.rb
+++ make_commits.rb
@@ -2,10 +2,17 @@ require 'csv'
 
 CSV.foreach("#{Dir.pwd}/commits.csv") do |row|
 
-  `echo \"triggering a commit\" >> test.txt`
-  `git add .`
-  `git commit -m \"#{row[0]}\"`
+  times = rand(5)
+  counter = 0
+
+  while counter < times do
+    `echo \"triggering a commit\" >> test.txt`
+    `git add .`
+    `git commit -m \"finished #{row[0]}\"`
+    counter += 1
+  end
 
 end
+
 
 `git push`
